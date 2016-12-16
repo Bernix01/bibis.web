15f8a2f671b8c50fb515f34d7b619d0e707ba4f8
Bernix01 <gbernal096@gmail.com>
2016-12-16 07:07:29 -0500

    bind data!

--- WebApplication5/FacturasDelDia.aspx.vb
+++ WebApplication5/FacturasDelDia.aspx.vb
@@ -7,11 +7,13 @@
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         fdbc = New FacturaDbContext()
         its = fdbc.getDelDia()
-        Label1.Text = If(its.Count() = 0, "No se encuentran facturas realizadas el día de hoy", "Hay " & its.Count() & "facturas del día de hoy.")
         If (its Is Nothing) Then
             Dim url = "/"
             Response.Redirect(url)
         End If
+        Label1.Text = If(its.Count() = 0, "No se encuentran facturas realizadas el día de hoy", "Hay " & its.Count() & "facturas del día de hoy.")
+        facs.DataSource = its
+        facs.DataBind()
     End Sub
 
 End Class
